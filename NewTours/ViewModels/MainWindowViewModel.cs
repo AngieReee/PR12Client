50916d813bb1ac71703eb91f13ac6dc5ddab2a75
СухареваАК <@106-03>
2025-01-14 13:17:17 +0300

    Добавьте файлы проекта.

---
+++ NewTours/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Avalonia.Controls;
+using ReactiveUI;
+
+namespace NewTours.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        UserControl uc = new ToursView();
+
+        public UserControl Uc { get => uc; set => this.RaiseAndSetIfChanged(ref uc, value); }
+    }
+}
---
+++ NewTours/Views/ToursView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace NewTours;
+
+public partial class ToursView : UserControl
+{
+    public ToursView()
+    {
+        InitializeComponent();
+    }
+}
